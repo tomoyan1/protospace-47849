fe8ae5f339f79fd818c9a7fac8a6c058fa88854e
tomoyan1 <133628017+tomoyan1@users.noreply.github.com>
2026-04-10 18:32:07 +0900

    チェック項目修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,7 +20,7 @@ class PrototypesController < ApplicationController
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
-      redirect_to root_path
+      redirect_to prototype_path(@prototype)
     else
       render :new, status: :unprocessable_entity
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+  validates :profile, presence: true
   validates :occupation, presence: true
   validates :position, presence: true
+  
 
   has_many :prototypes, dependent: :destroy
   has_many :comments
